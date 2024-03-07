ce0f671cf9a184650dac5330ac7be781e9553e7c
iftekher2108 <parvezkhan21082002@gmail.com>
2024-03-07 16:19:03 +0600

    vue project

---
+++ src/views/galleryView.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div>
+      <h1>gallery view</h1>
+    </div>
+  </main>
+</template>
---
+++ src/views/indexView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <main>
+    <h1>home view</h1>
+    <img src="/public/image/value.jpg" class="img-fluid" alt="image">
+  </main>
+</template>
